bb42ab3ebdfe7652cb32485e566d9ad25fb677d8
na-thang <absurdtakes@gmail.com>
2025-03-29 19:20:23 -0300

    Primeiro

---
+++ script.js
@@ -0,0 +1,57 @@
+const apiUrl = "https://api.coincap.io/v2/assets/bitcoin";
+
+async function obterDados() {
+    try {
+        const resposta = await fetch(apiUrl);
+        const dados = await resposta.json();
+
+        const nomeMoeda = dados.data.name;
+        const preco = parseFloat(dados.data.priceUsd).toFixed(2);
+        const variacao = parseFloat(dados.data.changePercent24Hr).toFixed(2);
+
+        document.getElementById("nomeMoeda").textContent = nomeMoeda;
+        document.getElementById("preco").textContent = `Preço: $${preco}`;
+        document.getElementById("variacao").textContent = `Variação: ${variacao}%`;
+
+        atualizarGrafico(preco);
+    } catch (error) {
+        console.error("Erro ao obter dados:", erro);
+    }
+}
+
+setInterval(obterDados, 5000);
+obterDados();
+
+const ctx = document.getElementById("grafico").getContext("2d");
+let dadosGrafico = {
+    labels: [],
+    datasets: [{
+        label: "Preço ($)",
+        borderColor: "#00ff00",
+        backgroundColor: "transparent",
+        data: []
+    }]
+};
+let grafico = new Chart(ctx, {
+    type: "line",
+    data: dadosGrafico,
+    options: {
+        responsive: true,
+        scales: {
+            x: { display: false },
+            y: { beginAtZero: false }
+        }
+    }
+})
+
+function atualizarGrafico(preco) {
+    if (dadosGrafico.labels.length > 10){
+        dadosGrafico.labels.shift();
+        dadosGrafico.datasets[0].data.shift();
+    }
+
+    dadosGrafico.labels.push("");
+    dadosGrafico.datasets[0].data.push(preco);
+
+    grafico.update();
+}
